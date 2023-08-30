ad3fb4cbb23023ddfaa446e8477a55505e08f69e
Mohd-Murtaza <mohd.murtaza0101@gmail.com>
2023-08-30 14:55:09 +0530

    now your both program are okay

--- prime.js
+++ prime.js
@@ -1,13 +1,8 @@
 let num = 10
-let count = 1
+let count = 0
 for(let i = 1 ; i <= num ; i++){
     if(num % i == 0){
         count++
     }
 }
-if(count == 2){
-    console.log("Prime")
-}
-else{
-    console.log("Not Prime")
-}
+(count == 2)?console.log("Prime"):console.log("Not Prime");
